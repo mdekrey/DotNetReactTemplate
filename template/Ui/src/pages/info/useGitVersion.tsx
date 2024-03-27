fa433a4941d3a68b9c8d70873a33274b99a8e69f
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-03-27 06:54:13 -0500

    Extract presentation component

--- template/Ui/src/pages/info/useGitVersion.tsx
+++ template/Ui/src/pages/info/useGitVersion.tsx
@@ -5,11 +5,19 @@ import { getInfo } from '@/utils/api/queries/getInfo';
 export function useGitVersion() {
 	const result = useSuspenseQuery(getInfo);
 	return useCallback(
-		() => (
-			<>
-				Hash: {result.data.gitHash}, Tag: {result.data.tag}
-			</>
-		),
-		[result],
+		() => <GitVersionPresentation data={result.data} />,
+		[result.data],
+	);
+}
+
+export function GitVersionPresentation({
+	data,
+}: {
+	data: { gitHash: string; tag: string };
+}) {
+	return (
+		<>
+			Hash: {data.gitHash}, Tag: {data.tag}
+		</>
 	);
 }
